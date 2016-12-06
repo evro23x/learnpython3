e4cf77bc93253e01ff39337044025d028b9ae35e
Evgeny Gurov <evro23x@mail.ru>
2016-12-06 23:38:39 +0300

    homework: operator if

---
+++ lesson1/hw/if.py
@@ -0,0 +1,15 @@
+def str_check(str1, str2):
+    if str1 == str2:
+        return 1
+    elif not str1 == str2 and str2 == 'learn':
+        return 3
+    elif not str1 == str2 and len(str1) > len(str2):
+        return 2
+
+
+while True:
+    string1 = (input("write first string: ").lower())
+    string2 = (input("write second string: ").lower())
+    print('Return value is: {}'.format(str_check(string1, string2)))
+    if string2 == "exit":
+        break
