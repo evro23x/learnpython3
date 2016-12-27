14f255969d7a1462c176c44a58d162ffe3d9cbeb
Evgeny Gurov <evro23x@mail.ru>
2016-12-28 01:23:08 +0300

    homework3: data from url & output in flask

--- lesson3/hw/get_data_from_url.py
+++ lesson3/hw/get_data_from_url.py
@@ -1,18 +1,13 @@
-# import json
-# import urllib
-# # import urllib.request
-#
-# url = "http://api.data.mos.ru/v1/datasets/2009/rows"
-# response = urllib.urlopen(url)
-# data = json.loads(response.read())
-# print(data)
-
-import json
-import urllib
+import requests
 
 
-data = urllib.urlopen("http://api.data.mos.ru/v1/datasets/2009/rows").read()
-output = json.loads(data)
-print(output)
-# print(data)
-#
+def get_weather(url):
+    result = requests.get(url)
+    if result.status_code == 200:
+        return result.json()
+    else:
+        print("Что-то пошло не так, сервер вернул status_code = {}".format(result.status_code))
+
+if __name__ == "__main__":
+    data = get_weather("http://api.data.mos.ru/v1/datasets/2009/rows")
+    print(data)
--- lesson3/hw/test_flask.py
+++ lesson3/hw/test_flask.py
@@ -1,9 +1,27 @@
 from flask import Flask
+from get_data_from_url import get_weather
 
 app = Flask(__name__)
-@app.route("/")
+
+
+@app.route("/names")
 def index():
-    return "Hi!"
+    data = get_weather("http://api.data.mos.ru/v1/datasets/2009/rows")
+    result = '<table><tr><th>Year</th><th>Month</th><th>Name</th></tr>'
+    for x in data:
+        # if x["Cells"]["Name"] == "Мария":
+        cells_year = str(x["Cells"]["Year"])
+        cells_month = str(x["Cells"]["Month"])
+        cells_name = str(x["Cells"]["Name"])
+        result += "<tr><td>{}</td><td>{}</td><td>{}</td></tr>".format(cells_year, cells_month, cells_name)
+    result += "</table>"
+    return result
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=5010, debug=True)
+
+
+# <table><tr><th>Заголовок колонки 1</th><th>Заголовок колонки 2</th><th>Заголовок колонки 3</th></tr>
+# <tr><td>Данные 1</td><td>Данные 2</td><td>Данные 3</td></tr>
+# </table>
